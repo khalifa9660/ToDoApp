734ceeb1c373ffe104ca517b53b89308bfcd8bea
khalifa Tambadou <khalifa.tambadou@perrenot.eu>
2023-10-05 13:55:36 +0200

    Implémentation des requêtes CRUD

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  public todos:any;
+  public activeTasks:boolean;
+  public newTodo:string = '';
 
-  ngOnInit(): void {
+  constructor(private todoService: TodoService) { }
+
+  getTodos(){
+    return this.todoService.get().then(todos =>{
+      this.todos = todos;
+      this.activeTasks = this.todos.filter(todo => !todo.isDone).length;
+    })
   }
 
+  ngOnInit() {
+    this.getTodos()
+  }
+
+  addTodo(){
+    this.todoService.add({ title: this.newTodo, isDone: false })
+    .then(() => {
+      return this.getTodos();
+    })
+    .then(() => {
+      this.newTodo = '';
+    })
+    .catch(error => {
+      console.error('Une erreur est survenue :', error);
+    });
+  }
+
+  updateTodo(todo, newValue){
+    todo.title = newValue;
+    return this.todoService.put(todo).then(()=>{
+      todo.editing = false
+      return this.getTodos();
+    })
+  }
+
+  destroyTodo(todo){
+    this.todoService.delete(todo).then(()=>{
+      return this.getTodos();
+    })
+  }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { promise } from 'protractor';
+
+const TODOS = [
+  {title: 'Maîtriser le CRUD', isDone: true},
+  {title: 'chercher à comprendre un maximum les choses', isDone: false},
+  {title: "Savoir dire que l'on ne sait pas", isDone: true},
+  {title: 'Finir mon projet RNCP', isDone: true},
+  {title:'Finir mes dossiers', isDone: false},
+  {title: 'Commencer à lire des livres', isDone: false},
+  {title: 'Arranger les choses que je suis capable de remettre sur de bon rails', isDone: false}
+] 
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +17,31 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   constructor() { }
+
+  get(){
+    return new Promise(resolve => resolve(TODOS))
+  }
+
+  add(data){
+    return new Promise(resolve =>{
+      TODOS.push(data);
+      resolve(data);
+    })
+  }
+
+  put(changed){
+    return new Promise(resolve=>{
+      const index = TODOS.findIndex(todo => todo === changed);
+      TODOS[index].title = changed.title;
+      resolve(changed);
+    })
+  }
+
+  delete(selected){
+    return new Promise(resolve =>{
+      const index = TODOS.findIndex(todo => todo === selected)
+      TODOS.splice(index, 1);
+      resolve(true)
+    })
+  }
 }
